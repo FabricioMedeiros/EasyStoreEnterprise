2cad479723be23e306e4b357e55567a15012a7ae
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-09-29 19:48:00 -0300

    Criacao da Model UserViewModels na WebApp

---
+++ src/web/ECE.WebApp.MVC/Models/UserViewModels.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECE.WebApp.MVC.Models
+{
+    public class UserRegister
+    {
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+        public string PasswordConfirmation { get; set; }
+    }
+
+    public class UserLogin
+    {
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
