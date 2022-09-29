f3d311f7af69aa90f0bb749c1e8aba45482e2ea8
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-09-29 19:58:19 -0300

    Criacao da Controle de Autenticacao e estrutura dos metodos que serao utilizados na Autenticacao (criacao de cadastro, log in e log out) da WebApp

---
+++ src/web/ECE.WebApp.MVC/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using ECE.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECE.WebApp.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(UserRegister userRegister)
+        {
+            if (!ModelState.IsValid) return View(userRegister);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return View(userLogin);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
