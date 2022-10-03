932ac41f04159b01ff71fef2c2f9cf1ca7d2434c
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-10-03 20:22:54 -0300

    Criacao das Models para deserializacao do Token de Autenticacao na WebApp

--- src/web/ECE.WebApp.MVC/Models/UserViewModels.cs
+++ src/web/ECE.WebApp.MVC/Models/UserViewModels.cs
@@ -31,4 +31,24 @@ namespace ECE.WebApp.MVC.Models
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; }
     }
+
+    public class UserResponseLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserToken UserToken { get; set; }
+    }
+
+    public class UserToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UserClaim> Claims { get; set; }
+    }
+
+    public class UserClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
 }
