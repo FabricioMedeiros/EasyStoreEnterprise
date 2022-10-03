05c77e0272f2e456b46ee57ba898b517c9496dd3
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-10-03 19:53:12 -0300

    Criacao da Interface e dos Servicos para consumir a API de Authenticao na Web App

---
+++ src/web/ECE.WebApp.MVC/Services/AuthenticationService.cs
@@ -0,0 +1,43 @@
+﻿using ECE.WebApp.MVC.Models;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ECE.WebApp.MVC.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthenticationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Login(UserLogin userLogin)
+        {
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(userLogin),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:44334/api/authentication/login", loginContent);
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<string> Register(UserRegister userRegister)
+        {
+            var registerContent = new StringContent(
+                 JsonSerializer.Serialize(userRegister),
+                 Encoding.UTF8,
+                 "application/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:44334/api/authentication/register", registerContent);
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
---
+++ src/web/ECE.WebApp.MVC/Services/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using ECE.WebApp.MVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECE.WebApp.MVC.Services
+{
+    public interface IAuthenticationService 
+    {
+        Task<string> Login(UserLogin userLogin);
+
+        Task<string> Register(UserRegister userRegister);
+    }
+}
