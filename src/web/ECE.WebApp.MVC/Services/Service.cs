b331ba089858c232f36272a44cf9d95ea6317dec
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-10-17 20:15:39 -0300

    Criacao dos Metodos para Verificar se a Resposta de Login e Registro possue erros e desserializar o retorno de erros na WebApp

---
+++ src/web/ECE.WebApp.MVC/Services/Service.cs
@@ -0,0 +1,51 @@
+﻿using ECE.WebApp.MVC.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ECE.WebApp.MVC.Services
+{
+    public abstract class Service
+    {
+        protected StringContent GetContent(object data)
+        {
+            return new StringContent(
+                                     JsonSerializer.Serialize(data),
+                                     Encoding.UTF8,
+                                     "application/json");
+        }
+
+        protected async Task<T> DeserializeObjectResponse<T>(HttpResponseMessage responseMessage)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), options);
+        }
+
+        protected bool CheckErrorsResponse(HttpResponseMessage response)
+        {
+            switch ((int)response.StatusCode)
+            {
+                case 401:
+                case 403:
+                case 404:
+                case 500:
+                    throw new CustomHttpRequestException(response.StatusCode);
+
+                case 400:
+                    return false;
+            }
+
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
+    }
+}
+
