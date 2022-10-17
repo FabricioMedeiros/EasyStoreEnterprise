f2d0eae77f1af8c9c7aa6ed57867befd52e78dd0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-10-17 20:19:47 -0300

    Adicionada a Propriedade ResponseResult na Model UserResponseLogin na WebApp

--- src/web/ECE.WebApp.MVC/Models/UserViewModels.cs
+++ src/web/ECE.WebApp.MVC/Models/UserViewModels.cs
@@ -38,6 +38,7 @@ namespace ECE.WebApp.MVC.Models
         public string AccessToken { get; set; }
         public double ExpiresIn { get; set; }
         public UserToken UserToken { get; set; }
+        public ResponseResult ResponseResult { get; set; }
     }
 
     public class UserToken
