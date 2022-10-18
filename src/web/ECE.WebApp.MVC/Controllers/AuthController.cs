24604671dfb5593367d15a8f49498584b764fa25
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-10-18 19:51:42 -0300

    Adicionado tratamento para realizar o Logoff do usuario na WebApp

--- src/web/ECE.WebApp.MVC/Controllers/AuthController.cs
+++ src/web/ECE.WebApp.MVC/Controllers/AuthController.cs
@@ -73,6 +73,7 @@ namespace ECE.WebApp.MVC.Controllers
         [Route("logout")]
         public async Task<IActionResult> Logout()
         {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Index", "Home");
         }
 
