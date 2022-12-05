bafc077d38b41d39d77487b52221bc92c9a9a373
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-05 20:05:09 -0300

    Criacao da Controller Catalog na WebApp

---
+++ src/web/ESE.WebApp.MVC/Controllers/CatalogController.cs
@@ -0,0 +1,36 @@
+﻿using ESE.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Controllers
+{
+    public class CatalogController : MainController
+    {
+        private readonly ICatalogService _catalogService;
+
+        public CatalogController(ICatalogService catalogService)
+        {
+            _catalogService = catalogService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("products")]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _catalogService.Get();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        [Route("product-details/{id}")]
+        public async Task<IActionResult> ProductDetail(Guid id)
+        {
+            var product = await _catalogService.GetById(id);
+
+            return View(product);
+        }
+    }
+}
