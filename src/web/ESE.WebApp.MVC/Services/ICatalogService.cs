4b29daa8289103af53fbba469a33b768a25da290
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-05 19:48:07 -0300

    Criacao da Interface ICatalogService na WebApp

---
+++ src/web/ESE.WebApp.MVC/Services/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿using ESE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Services
+{
+    public interface ICatalogService
+    {
+       Task<IEnumerable<ProductViewModel>> Get();
+       Task<ProductViewModel> GetById(Guid id);
+    }
+}
