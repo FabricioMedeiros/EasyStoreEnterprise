6d3a97b67cc4621925fb53cac1867a0b5daaef79
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-05 21:08:59 -0300

    Adicionada a property Id na ProductViewModel da WebApp

--- src/web/ESE.WebApp.MVC/Models/ProductViewModel.cs
+++ src/web/ESE.WebApp.MVC/Models/ProductViewModel.cs
@@ -4,6 +4,7 @@ namespace ESE.WebApp.MVC.Models
 {
     public class ProductViewModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public bool Active { get; set; }
