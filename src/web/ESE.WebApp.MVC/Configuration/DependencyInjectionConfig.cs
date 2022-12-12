33157aec8a351cc9fb03818f5dc09f8cc678f84b
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-12 19:41:49 -0300

    Implementacao do Polly para tratativa de erros de conexao Http na WebApp

--- src/web/ESE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/ESE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -3,6 +3,11 @@ using ESE.WebApp.MVC.Services;
 using ESE.WebApp.MVC.Services.Handlers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using System;
+using System.Net.Http;
 
 namespace ESE.WebApp.MVC.Configuration
 {
@@ -15,11 +20,35 @@ namespace ESE.WebApp.MVC.Configuration
             services.AddHttpClient<IAuthService, AuthService>();
 
             services.AddHttpClient<ICatalogService, CatalogService>().
-                     AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+                     AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                     .AddPolicyHandler(PollyExtensions.WaitTry())
+                     .AddTransientHttpErrorPolicy(
+                          p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30))); ;
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
         }
+    }
 
+    public class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> WaitTry()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10),
+                }, (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            return retry;
+        }
     }
 } 
