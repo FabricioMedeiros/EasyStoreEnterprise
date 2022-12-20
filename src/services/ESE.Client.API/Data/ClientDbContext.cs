6f5e7ddfe854bea31286950923a42105ed624850
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-20 20:32:42 -0300

    Instalacao do EntityFramework e criacao do ClientDbContext na API de Clientes

---
+++ src/services/ESE.Client.API/Data/ClientDbContext.cs
@@ -0,0 +1,37 @@
+﻿using ESE.Client.API.Models;
+using ESE.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.Client.API.Data
+{
+    public class ClientDbContext : DbContext, IUnitOfWork
+    {
+        public ClientDbContext(DbContextOptions<ClientDbContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Models.Client> Clients { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientDbContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
