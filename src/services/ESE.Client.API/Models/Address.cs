3e3e19a726e36be9f5cf68f60bbefb8b95fb6e9f
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-20 19:54:14 -0300

    Criacao da API de Clientes e das Classes base

---
+++ src/services/ESE.Client.API/Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ESE.Client.API.Models
+{
+    public class Address
+    {
+       public Guid ClientId { get; set; }
+       public string Street { get; set; }
+       public string Number { get; set; }
+       public string Complement { get; set; }
+       public string Neighborhood { get; set; }
+       public string City { get; set; }
+       public string State { get; set; }
+       public string ZipCode { get; set; }
+
+       // EF Relation
+       public Client Client { get; set; }
+
+       public Address(string street, string number, string complement, string neighborhood, string city, string state, string zipcode)
+       {
+         Street = street;
+         Number = number;
+         Complement = complement;
+         Neighborhood = neighborhood;
+         City = city;
+         State = state;
+         ZipCode = zipcode;
+       }
+    }
+}
---
+++ src/services/ESE.Client.API/Models/Client.cs
@@ -0,0 +1,37 @@
+﻿using ESE.Core.DomainObjects;
+using System;
+
+
+namespace ESE.Client.API.Models
+{
+    public class Client : Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
+        public bool Active { get; private set; }
+        public Address Address { get; private set; }
+
+        // EF Relation
+        protected Client() { }
+
+        public Client(Guid id, string name, string email, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
+            Active = true;
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void SetAddress(Address address)
+        {
+            Address = address;
+        }
+    }
+}
