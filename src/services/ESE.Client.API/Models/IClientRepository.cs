1af60e9ba33478e5995a8d2add2a246e9476b905
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-26 19:39:32 -0300

    Criacao da interface IClientRepository que ira definir os metodos que serao implementados na classe ClientRepository da API de Clientes

---
+++ src/services/ESE.Client.API/Models/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using ESE.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ESE.Clients.API.Models
+{
+    public interface IClientRepository : IRepository<Client>
+    {
+        void Add(Client client);
+
+        Task<IEnumerable<Client>> GetAll();
+        Task<Client> GetByCPF(string cpf);
+    }
+}
