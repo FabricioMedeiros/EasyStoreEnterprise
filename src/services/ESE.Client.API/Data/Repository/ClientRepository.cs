78a2a965979e6e3c803647bade480e5ff54602ca
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-26 20:07:03 -0300

    Criacao da Classe ClientRepository que implementa a Interface IClientRepository na API de Clientes

---
+++ src/services/ESE.Client.API/Data/Repository/ClientRepository.cs
@@ -0,0 +1,40 @@
+﻿using ESE.Clients.API.Models;
+using ESE.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ESE.Clients.API.Data.Repository
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly ClientDbContext _context;
+
+        public ClientRepository(ClientDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public async Task<IEnumerable<Client>> GetAll()
+        {
+            return await _context.Clients.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Client> GetByCpf(string cpf)
+        {
+            return _context.Clients.FirstOrDefaultAsync(c => c.Cpf.Number == cpf);
+        }
+
+        public void Add(Client client)
+        {
+            _context.Clients.Add(client);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
