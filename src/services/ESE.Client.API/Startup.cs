523fcc3a899af65dc85f26dee5f8cd28877f80df
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-26 20:17:36 -0300

    refatoracao da Startup da API de Clientes

--- src/services/ESE.Client.API/Startup.cs
+++ src/services/ESE.Client.API/Startup.cs
@@ -32,21 +32,7 @@ namespace ESE.Clients.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApiConfiguration(env);
         }
     }
 }
