9c6ce7268ab4be0a5f97d96849838e762b6823cd
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2022-12-26 20:16:45 -0300

    Criacao da classe de configuracao de Injecao de Dependencia na API de Clientes

---
+++ src/services/ESE.Client.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using ESE.Clients.API.Data;
+using ESE.Clients.API.Data.Repository;
+using ESE.Clients.API.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ESE.Clients.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<ClientDbContext>();
+        }
+    }
+}
