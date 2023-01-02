44f4a28ecd92aa38233f98c04b274456cf572203
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-02 19:01:47 -0300

    Adicionada estrutura basica para trabalhar com Commands para Registro de Clientes na API de Clientes

---
+++ src/services/ESE.Client.API/Application/Commands/ClientCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.Clients.API.Application.Commands
+{
+    public class ClientCommandHandler
+    {
+
+    }
+}
+
---
+++ src/services/ESE.Client.API/Application/Commands/RegisterClientCommand.cs
@@ -0,0 +1,24 @@
+﻿using ESE.Core.Messages;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ESE.Clients.API.Application.Commands
+{
+    public class RegisterClientCommand : Command
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public RegisterClientCommand(Guid id, string name, string email, string cpf)
+        {
+            AggregateId = id;
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }        
+    }
+}
