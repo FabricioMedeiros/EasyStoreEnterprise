8b817a2c3f6fb92775020b3676b4068ae6bf4976
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-02 18:58:36 -0300

    Adicionada estrutura basica para trabalhar com Commands (CQRS) no Core

---
+++ src/buildingblocks/ESE.Core/Messages/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ESE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+ 
+}
