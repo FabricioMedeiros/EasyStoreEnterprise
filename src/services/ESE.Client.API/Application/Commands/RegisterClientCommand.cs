d3c902b1b6f02c451e7a3dbed343925a48dc0b51
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-03 09:34:32 -0300

    Adicionados os tratamentos de validacao de comandos do registro de clientes na API de Clientes

--- src/services/ESE.Client.API/Application/Commands/RegisterClientCommand.cs
+++ src/services/ESE.Client.API/Application/Commands/RegisterClientCommand.cs
@@ -1,7 +1,6 @@
 ﻿using ESE.Core.Messages;
+using FluentValidation;
 using System;
-using System.ComponentModel.DataAnnotations;
-
 
 namespace ESE.Clients.API.Application.Commands
 {
@@ -19,6 +18,44 @@ namespace ESE.Clients.API.Application.Commands
             Name = name;
             Email = email;
             Cpf = cpf;
-        }        
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new RegisterClientValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+
+        public class RegisterClientValidation : AbstractValidator<RegisterClientCommand>
+        {
+            public RegisterClientValidation()
+            {
+                RuleFor(c => c.Id)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do cliente inválido");
+
+                RuleFor(c => c.Name)
+                    .NotEmpty()
+                    .WithMessage("O nome do cliente não foi informado");
+
+                RuleFor(c => c.Cpf)
+                    .Must(ValidateCpf)
+                    .WithMessage("O CPF informado não é válido.");
+
+                RuleFor(c => c.Email)
+                    .Must(ValidateEmail)
+                    .WithMessage("O e-mail informado não é válido.");
+            }
+
+            protected static bool ValidateCpf(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validate(cpf);
+            }
+
+            protected static bool ValidateEmail(string email)
+            {
+                return Core.DomainObjects.Email.Validate(email);
+            }
+        }
     }
 }
