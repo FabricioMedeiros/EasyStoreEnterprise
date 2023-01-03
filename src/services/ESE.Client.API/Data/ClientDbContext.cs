d7ef3eb1ffe0c743eb03dcdb867ae8199a886af0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-03 20:13:23 -0300

    Adicionada a extensao do Mediator para extrair as Notificacoes (Eventos) das Entidades e tratamento para publicar os Eventos caso o Commit da entidade tenha sido realizado com sucesso

--- src/services/ESE.Client.API/Data/ClientDbContext.cs
+++ src/services/ESE.Client.API/Data/ClientDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using ESE.Clients.API.Models;
 using ESE.Core.Data;
+using ESE.Core.DomainObjects;
+using ESE.Core.Mediator;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,8 +10,10 @@ namespace ESE.Clients.API.Data
 {
     public class ClientDbContext : DbContext, IUnitOfWork
     {
-        public ClientDbContext(DbContextOptions<ClientDbContext> options) : base(options)
+        private readonly IMediatorHandler _mediatRHandler;
+        public ClientDbContext(DbContextOptions<ClientDbContext> options, IMediatorHandler mediatRHandler) : base(options)
         {
+            _mediatRHandler = mediatRHandler;
             ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
             ChangeTracker.AutoDetectChangesEnabled = false;
         }
@@ -30,8 +34,36 @@ namespace ESE.Clients.API.Data
         }
 
         public async Task<bool> Commit()
+        { 
+            var success = await base.SaveChangesAsync() > 0;
+
+            if (success) await _mediatRHandler.PublishEvents(this);
+
+            return success;
+        }        
+    }
+
+    public static class MediatorExtension
+    {
+        public static async Task PublishEvents<T>(this IMediatorHandler mediator, T ctx) where T : DbContext
         {
-            return await base.SaveChangesAsync() > 0;
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.Notifications != null && x.Entity.Notifications.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Notifications)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.ClearEvents());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) => {
+                    await mediator.PublishEvent(domainEvent);
+                });
+
+            await Task.WhenAll(tasks);
         }
     }
 }
