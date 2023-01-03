812b236f845cebf3bcd88ea0fa4ec00c481058ef
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-03 19:35:43 -0300

    Adiconadas as Classes ClientRegisteredEvent e ClientEventHandler para implementar e manipular o Evento de registro dos Clientes na API de Clientes

---
+++ src/services/ESE.Client.API/Application/Events/ClientEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESE.Clients.API.Application.Events
+{
+    public class ClientEventHandler : INotificationHandler<ClientRegisteredEvent>
+    {
+        public Task Handle(ClientRegisteredEvent notification, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
