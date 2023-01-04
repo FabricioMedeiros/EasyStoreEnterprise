ed2bedaeb6d9ba3a1dd319bf6cb993b0ffb1afaf
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-04 16:11:04 -0300

    Extracao da MainController da API de Autenticacao para o projeto WebAPI Core

--- src/buildingblocks/ESE.WebAPI.Core/Controllers/MainController.cs
+++ src/buildingblocks/ESE.WebAPI.Core/Controllers/MainController.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace ESE.Authentication.API.Controllers
+namespace ESE.WebAPI.Core.Controllers
 {
     [ApiController]
     public abstract class MainController : Controller
