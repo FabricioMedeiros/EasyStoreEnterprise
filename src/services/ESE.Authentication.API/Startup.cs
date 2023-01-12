789f670ed44b3372221a0d01bf82ac6fc37cf5f1
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-12 20:44:04 -0300

    Configuracao do registro do MessageBus na API de Autenticacao

--- src/services/ESE.Authentication.API/Startup.cs
+++ src/services/ESE.Authentication.API/Startup.cs
@@ -44,6 +44,8 @@ namespace ESE.Authentication.API
             services.AddApiConfiguration();
 
             services.AddSwaggerConfiguration();
+
+            services.AddMessageBusConfiguration(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
