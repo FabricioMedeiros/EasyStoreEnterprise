39b28dd30d8eeab21d0cd7905ceeac1aced85506
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-16 19:16:57 -0300

    Refatoracao do MessageBus na API de Clientes

--- src/services/ESE.Client.API/Services/RegisterClientIntegrationHandle.cs
+++ src/services/ESE.Client.API/Services/RegisterClientIntegrationHandle.cs
@@ -1,5 +1,4 @@
-﻿using EasyNetQ;
-using ESE.Clients.API.Application.Commands;
+﻿using ESE.Clients.API.Application.Commands;
 using ESE.Core.Mediator;
 using ESE.Core.Messages.Itegration;
 using FluentValidation.Results;
@@ -8,29 +7,29 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using ESE.MessageBus;
 
 namespace ESE.Clients.API.Services
 {
     public class RegisterClientIntegrationHandle : BackgroundService
     {
-        private IBus _bus;
+        private IMessageBus _bus;
         private readonly IServiceProvider _serviceProvider;
 
-        public RegisterClientIntegrationHandle(IServiceProvider serviceProvider)
+        public RegisterClientIntegrationHandle(IServiceProvider serviceProvider, IMessageBus bus)
         {
             _serviceProvider = serviceProvider;
+            _bus = bus;
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-           _bus = RabbitHutch.CreateBus("host=localhost:5672");
-
-           _bus.RespondAsync<UserRegisteredIntegrationEvent, ResponseMessage>(async request =>
-              new ResponseMessage(await RegisterClient(request)));
+            _bus.RespondAsync<UserRegisteredIntegrationEvent, ResponseMessage>(async request =>
+              await RegisterClient(request));
 
             return Task.CompletedTask;
         }
 
-        private async Task<ValidationResult> RegisterClient(UserRegisteredIntegrationEvent message)
+        private async Task<ResponseMessage> RegisterClient(UserRegisteredIntegrationEvent message)
         {
             var clientCommand = new RegisterClientCommand(message.Id, message.Name, message.Email, message.Cpf);
             ValidationResult success;
@@ -41,7 +40,7 @@ namespace ESE.Clients.API.Services
                 success = await mediator.SendCommand(clientCommand);
             }
 
-            return success;
+            return new ResponseMessage(success);
         }
     }
 }
