8c6d0e676f5047275d03385c6bb2f1240213c6a4
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-17 20:44:56 -0300

    Criacao do DbContext da API de Carrinho

---
+++ src/services/ESE.Cart.API/Data/CartDbContext.cs
@@ -0,0 +1,38 @@
+﻿using ESE.Cart.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ESE.Cart.API.Data
+{
+    public class CartDbContext : DbContext
+    {
+        public CartDbContext(DbContextOptions<CartDbContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<CartItem> CartItens { get; set; }
+        public DbSet<CartClient> CartClients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+                       
+
+            modelBuilder.Entity<CartClient>()
+                .HasIndex(c => c.ClientId)
+                .HasName("IDX_Client");
+
+            modelBuilder.Entity<CartClient>()
+                .HasMany(c => c.Items)
+                .WithOne(i => i.CartClient)
+                .HasForeignKey(c => c.CartId);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+        }
+    }
+}
