7f8fceade250896b34e770fcae9397527e3afef2
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-19 14:40:57 -0300

    Criacao da classe de Configuracao de Injecao de Dependencia da API de Carriho

---
+++ src/services/ESE.Cart.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using ESE.Cart.API.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ESE.Cart.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<CartDbContext>();
+        }
+    }
+}
