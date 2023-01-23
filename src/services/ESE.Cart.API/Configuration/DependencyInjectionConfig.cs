83a5d3ffdcf368cfbf42b3c310a76ffbf6edd697
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-23 19:54:09 -0300

    Registrando a Dependencia do AspNetUser na WebApp

--- src/services/ESE.Cart.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/ESE.Cart.API/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using ESE.Cart.API.Data;
+using ESE.WebAPI.Core.User;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ESE.Cart.API.Configuration
@@ -7,6 +9,8 @@ namespace ESE.Cart.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
             services.AddScoped<CartDbContext>();
         }
     }
