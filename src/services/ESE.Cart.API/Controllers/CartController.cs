2b7d2591ecf5b02935ebeb81693516ed2ca7674b
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-30 19:28:05 -0300

    Criacao da Controller de Carrinho e do Metodo Get para retornar o Carrinho do Cliente na API de Carrinho

---
+++ src/services/ESE.Cart.API/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+﻿using ESE.Cart.API.Data;
+using ESE.Cart.API.Models;
+using ESE.WebAPI.Core.Controllers;
+using ESE.WebAPI.Core.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace ESE.Cart.API.Controllers
+{
+    public class CartController : MainController
+    {
+        private readonly IAspNetUser _user;
+        private readonly CartDbContext _context;
+
+        public CartController(IAspNetUser user, CartDbContext context)
+        {
+            _user = user;
+            _context = context;
+        }
+
+        [HttpGet("cart")]
+        public async Task<CartClient> GetCart()
+        {
+            return await GetCartClient() ?? new CartClient();
+        }
+
+        private async Task<CartClient> GetCartClient()
+        {
+            return await _context.CartClients
+                .Include(c => c.Items)
+                .FirstOrDefaultAsync(c => c.ClientId == _user.GetUserId());
+        }
+
+    }
+}
