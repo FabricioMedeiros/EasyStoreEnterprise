fc5a08b78f3860c97406934e0e167bb8916d565d
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-30 20:07:53 -0300

    Criacao da Classe Aninhada CartItemValidation para Validar o Item do Carrinho de Compra, na API de Carrinho

--- src/services/ESE.Cart.API/Models/CartItem.cs
+++ src/services/ESE.Cart.API/Models/CartItem.cs
@@ -38,6 +38,32 @@ namespace ESE.Cart.API.Models
         {
             Quantity = units;
         }
-        
+
+        public class CartItemValidation : AbstractValidator<CartItem>
+        {
+            public CartItemValidation()
+            {
+                RuleFor(c => c.ProductId)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do produto inválido");
+
+                RuleFor(c => c.Name)
+                    .NotEmpty()
+                    .WithMessage("O nome do produto não foi informado");
+
+                RuleFor(c => c.Quantity)
+                    .GreaterThan(0)
+                    .WithMessage(item => $"A quantidade miníma para o {item.Name} é 1");
+
+                RuleFor(c => c.Quantity)
+                    .LessThanOrEqualTo(CartClient.MAX_QUANTITY_ITEMS)
+                    .WithMessage(item => $"A quantidade máxima do {item.Name} é {CartClient.MAX_QUANTITY_ITEMS}");
+
+                RuleFor(c => c.Price)
+                    .GreaterThan(0)
+                    .WithMessage(item => $"O valor do {item.Name} precisa ser maior que 0");
+            }
+        }
+
     }
 }
