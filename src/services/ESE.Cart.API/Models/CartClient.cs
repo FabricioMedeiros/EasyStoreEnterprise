255758ea657b0ecf89e9ba93ba5ebd09609e6329
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-01-30 20:35:32 -0300

    Criacao dos metodos na Classe do Carrinho do Cliente para calcular o total do carrinho, verificar se um item ja existe no carrinho, e para adicionar o item ao carrinho de compra, na API de Carrinho

--- src/services/ESE.Cart.API/Models/CartClient.cs
+++ src/services/ESE.Cart.API/Models/CartClient.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ESE.Cart.API.Models
 {
     public class CartClient
     {
+        internal const int MAX_QUANTITY_ITEMS = 5;
         public Guid Id { get; set; }
         public Guid ClientId { get; set; }
         public decimal TotalPrice { get; set; }
@@ -17,6 +19,39 @@ namespace ESE.Cart.API.Models
         }
 
         public CartClient() { }
+
+        internal void CalculateTotalValueCart()
+        {
+            TotalPrice = Items.Sum(p => p.CalculateValue());
+        }
+
+        internal bool ItemExistsCart(CartItem item)
+        {
+            return Items.Any(p => p.ProductId == item.ProductId);
+        }
+        internal CartItem GetProdutById(Guid productId)
+        {
+            return Items.FirstOrDefault(p => p.ProductId == productId);
+        }
+
+        internal void AddItem(CartItem item)
+        {
+            if (!item.IsValid()) return;
+
+            item.linkCart(Id);
+
+            if (ItemExistsCart(item))
+            {
+                var itemExists = GetProdutById(item.ProductId);
+                itemExists.AddUnits(item.Quantity);
+
+                item = itemExists;
+                Items.Remove(itemExists);
+            }
+
+            Items.Add(item);
+            CalculateTotalValueCart();
+        }
 
     }
 
