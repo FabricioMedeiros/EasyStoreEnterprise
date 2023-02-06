6e69c46fb5cce5f3aaec3a980cf5a33e85646fcf
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-02-06 19:35:13 -0300

    Criacao da Interface de Servicos do Carrinho na WebApp

---
+++ src/web/ESE.WebApp.MVC/Services/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using ESE.WebApp.MVC.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Services
+{
+    interface ICartService
+    {
+        Task<CartViewModel> GetCart();
+        Task<ResponseResult> AddItemCart(ItemProductViewModel product);
+        Task<ResponseResult> UpdateItemCart(Guid productId, ItemProductViewModel product);
+        Task<ResponseResult> RemoveItemCart(Guid productId);
+    }
+}
