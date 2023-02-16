154e9a674c2142db079a889739bdc65b7c83f75a
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-02-16 11:07:35 -0300

    Remocao de comentarios na Startup da API de Carrinhos

--- src/services/ESE.Cart.API/Startup.cs
+++ src/services/ESE.Cart.API/Startup.cs
@@ -26,8 +26,6 @@ namespace ESE.Cart.API
 
             Configuration = builder.Build();
         }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApiConfiguration(Configuration);
@@ -39,7 +37,6 @@ namespace ESE.Cart.API
             services.RegisterServices();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwaggerConfiguration();
