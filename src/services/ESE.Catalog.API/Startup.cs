6a6d4bbc652780fd87c0814c2df292616f1777e3
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-04-05 19:58:55 -0300

    Remocao das extensoes nao utilizadas

--- src/services/ESE.Catalog.API/Startup.cs
+++ src/services/ESE.Catalog.API/Startup.cs
@@ -1,21 +1,9 @@
 using ESE.Catalog.API.Configuration;
-using ESE.Catalog.API.Data;
-using ESE.Catalog.API.Data.Repository;
-using ESE.Catalog.API.Models;
 using ESE.WebAPI.Core.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ESE.Catalog.API
 {
