4a93e33c721bb65daa02bac88af5db790ee7f179
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-04-05 19:50:20 -0300

    Criacao do View Component para o Carrinho de Compras na WebApp

---
+++ src/web/ESE.WebApp.MVC/Extensions/CartViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using ESE.WebApp.MVC.Models;
+using ESE.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Extensions
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService _cartService;
+
+        public CartViewComponent(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _cartService.GetCart() ?? new CartViewModel());
+        }
+    }
+}
