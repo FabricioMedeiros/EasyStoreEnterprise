30f306b1c2d07df8e2f628b5b4fadab70e1e87b4
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-04-10 15:29:47 -0300

    Criacao da Controller de Carrinho na API Gateway de Compra

--- src/apigateways/ESE.Bff.Shopping/Controllers/CartController.cs
+++ src/apigateways/ESE.Bff.Shopping/Controllers/CartController.cs
@@ -1,49 +1,58 @@
-﻿using ESE.WebAPI.Core.Controllers;
+﻿using ESE.Bff.Shopping.Services;
+using ESE.WebAPI.Core.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace ESE.Bff.Shopping.Controllers
 {
-    public class CartController : Controller
+
+    [Authorize]
+    public class CartController : MainController
     {
-        [Authorize]
-        public class CarrinhoController : MainController
+        private readonly ICartService _cartService;
+        private readonly ICatalogService _catalogService;
+
+        public CartController(ICartService cartService, ICatalogService catalogService)
         {
-            [HttpGet]
-            [Route("shopping/cart")]
-            public async Task<IActionResult> Index()
-            {
-                return CustomResponse();
-            }
+            _cartService = cartService;
+            _catalogService = catalogService;
+        }
 
-            [HttpGet]
-            [Route("shopping/quantity-cart")]
-            public async Task<IActionResult> GetQuantityCart()
-            {
-                return CustomResponse();
-            }
+        [HttpGet]
+        [Route("shopping/cart")]
+        public async Task<IActionResult> Index()
+        {
+            return CustomResponse();
+        }
 
-            [HttpPost]
-            [Route("shopping/cart/items")]
-            public async Task<IActionResult> AddItemCart()
-            {
-                return CustomResponse();
-            }
+        [HttpGet]
+        [Route("shopping/quantity-cart")]
+        public async Task<IActionResult> GetQuantityCart()
+        {
+            return CustomResponse();
+        }
 
-            [HttpPut]
-            [Route("shopping/cart/items/{productId}")]
-            public async Task<IActionResult> UpdateItemCart()
-            {
-                return CustomResponse();
-            }
+        [HttpPost]
+        [Route("shopping/cart/items")]
+        public async Task<IActionResult> AddItemCart()
+        {
+            return CustomResponse();
+        }
 
-            [HttpDelete]
-            [Route("shopping/cart/items/{productId}")]
-            public async Task<IActionResult> DeleteItemCart()
-            {
-                return CustomResponse();
-            }
+        [HttpPut]
+        [Route("shopping/cart/items/{productId}")]
+        public async Task<IActionResult> UpdateItemCart()
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete]
+        [Route("shopping/cart/items/{productId}")]
+        public async Task<IActionResult> DeleteItemCart()
+        {
+            return CustomResponse();
         }
     }
 }
+
