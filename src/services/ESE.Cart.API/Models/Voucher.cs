6b11c7481f0b961bc16b92d3f65ffeea596910c2
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-04-24 22:17:34 -0300

    Criacao da model Voucher na API de Carrinho

---
+++ src/services/ESE.Cart.API/Models/Voucher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.Cart.API.Models
+{
+    public class Voucher
+    {
+        public decimal? Percent { get; set; }
+        public decimal? DiscountValue { get; set; }
+        public string Code { get; set; }
+        public TypeDiscountVoucher TypeDiscount { get; set; }
+    }
+
+    public enum TypeDiscountVoucher
+    {
+        Percentage = 0,
+        Value = 1
+    }
+}
