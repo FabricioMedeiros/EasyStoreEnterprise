d8e31456b83aec0e6e7faa11f58efb3fe70612f6
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-04-27 21:27:09 -0300

    Criacao das Migrations de Pedido e Itens de Pedido

--- src/services/ESE.Order.Infra/Migrations/20230428001804_Orders.Designer.cs
+++ src/services/ESE.Order.Infra/Migrations/20230428001804_Orders.Designer.cs
@@ -4,14 +4,16 @@ using ESE.Orders.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ESE.Orders.Infra.Migrations
 {
     [DbContext(typeof(OrderDbContext))]
-    partial class OrderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230428001804_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
