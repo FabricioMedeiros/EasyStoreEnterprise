024bc7449b2e055df43817fc2d26eae6df0b99a6
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-11 20:18:07 -0300

    Criacao da Controller de Clientes na WebApp

---
+++ src/web/ESE.WebApp.MVC/Controllers/ClientController.cs
@@ -0,0 +1,31 @@
+﻿using ESE.WebApp.MVC.Models;
+using ESE.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Controllers
+{
+    [Authorize]
+    public class ClienteController : MainController
+    {
+        private readonly IClientService _clientService;
+
+        public ClienteController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewAddress(AddressViewModel address)
+        {
+            var response = await _clientService.AddAddress(address);
+
+            if (HasErrorsResponse(response)) TempData["Errors"] =
+                ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
+
+            return RedirectToAction("DeliveryAddress", "Order");
+        }
+    }
+}
