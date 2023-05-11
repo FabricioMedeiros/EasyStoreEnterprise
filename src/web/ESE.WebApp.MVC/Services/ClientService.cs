4b747dfa90e4a0a928f09d5b88bfcd7341f10880
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-11 20:11:58 -0300

    Criacao do servico de Clientes na WebApp

---
+++ src/web/ESE.WebApp.MVC/Services/ClientService.cs
@@ -0,0 +1,50 @@
+﻿using ESE.Core.Communication;
+using ESE.WebApp.MVC.Extensions;
+using ESE.WebApp.MVC.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Services
+{
+    public interface IClientService
+    {
+        Task<AddressViewModel> GetAddress();
+        Task<ResponseResult> AddAddress(AddressViewModel address);
+    }
+
+    public class ClienteService : Service, IClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClienteService(HttpClient httpClient, IOptions<AppSettings> settings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(settings.Value.ClientUrl);
+        }
+
+        public async Task<AddressViewModel> GetAddress()
+        {
+            var response = await _httpClient.GetAsync("/client/address/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            CheckErrorsResponse(response);
+
+            return await DeserializeObjectResponse<AddressViewModel>(response);
+        }
+
+        public async Task<ResponseResult> AddAddress(AddressViewModel address)
+        {
+            var addressContent = JsonSerialize(address);
+
+            var response = await _httpClient.PostAsync("/client/address/", addressContent);
+
+            if (!CheckErrorsResponse(response)) return await DeserializeObjectResponse<ResponseResult>(response);
+
+            return ReturnOk();
+        }
+    }
+}
