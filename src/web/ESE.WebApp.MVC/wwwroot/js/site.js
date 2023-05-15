b0eee9a5fd46bdb2c4cf5c9b8a573522c0ac5ffb
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-15 20:34:41 -0300

    Criacao da funcao de Busca de CEP que sera utilizada nas Views de Endereco na WebApp

--- src/web/ESE.WebApp.MVC/wwwroot/js/site.js
+++ src/web/ESE.WebApp.MVC/wwwroot/js/site.js
@@ -1,4 +1,47 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function SearchZipCode() {
+    $(document).ready(function () {
 
-// Write your JavaScript code.
+        function clear_fields() {
+            $("#Address_Street").val("");
+            $("#Address_Neighborhood").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
+        }
+
+        $("#Address_ZipCode").blur(function () {
+            var zipcode = $(this).val().replace(/\D/g, '');
+
+            if (zipcode != "") {
+                var checkzipcode = /^[0-9]{8}$/;
+
+                if (checkzipcode.test(zipcode)) {
+                    $("#Address_Street").val("...");
+                    $("#Address_Neighborhood").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_State").val("...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + zipcode + "/json/?callback=?",
+                        function (data) {
+                            if (!("erro" in data)) {
+                                $("#Address_Street").val(data.logradouro);
+                                $("#Address_Neighborhood").val(data.bairro);
+                                $("#Address_City").val(data.localidade);
+                                $("#Address_State").val(data.uf);
+                            }
+                            else {
+                                clear_fields();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                }
+                else {
+                    clear_fields();
+                    alert("Formato de CEP inválido.");
+                }
+            }
+            else {
+                clear_fields();
+            }
+        });
+    });
+}
