941389c1bbe238a5acd29ff71711d81125fd17a9
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-16 20:26:52 -0300

    Criacao dos metodos de pedido na Controller de Pedidos da WebApp

--- src/web/ESE.WebApp.MVC/Controllers/OrderController.cs
+++ src/web/ESE.WebApp.MVC/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using ESE.WebApp.MVC.Services;
+﻿using ESE.WebApp.MVC.Models;
+using ESE.WebApp.MVC.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,6 +28,52 @@ namespace ESE.WebApp.MVC.Controllers
             var order = _shoppingBffService.MapToOrder(cart, address);
 
             return View(order);
+        }
+
+        [HttpGet]
+        [Route("payment")]
+        public async Task<IActionResult> Pagamento()
+        {
+            var cart = await _shoppingBffService.GetCart();
+            if (cart.Items.Count == 0) return RedirectToAction("Index", "Cart");
+
+            var order = _shoppingBffService.MapToOrder(cart, null);
+
+            return View(order);
+        }
+
+        [HttpPost]
+        [Route("checkout-order")]
+        public async Task<IActionResult> FinalizarPedido(OrderTransactionViewModel orderTransaction)
+        {
+            if (!ModelState.IsValid) 
+              return View("Payment", _shoppingBffService.MapToOrder( await _shoppingBffService.GetCart(), null));
+
+            var response = await _shoppingBffService.CheckoutOrder(orderTransaction);
+
+            if (HasErrorsResponse(response))
+            {
+                var cart = await _shoppingBffService.GetCart();
+                if (cart.Items.Count == 0) return RedirectToAction("Index", "Cart");
+
+                var orderMap = _shoppingBffService.MapToOrder(cart, null);
+                return View("Payment", orderMap);
+            }
+
+            return RedirectToAction("OrderCompleted");
+        }
+
+        [HttpGet]
+        [Route("order-completed")]
+        public async Task<IActionResult> OrderCompleted()
+        {
+            return View("ConfirmationOrder", await _shoppingBffService.GetLastOrder());
+        }
+
+        [HttpGet("my-orders")]
+        public async Task<IActionResult> MyOrders()
+        {
+            return View(await _shoppingBffService.GetListByClientId());
         }
     }
 }
