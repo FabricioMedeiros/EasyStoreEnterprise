42ec6a2399d391cb7917961be0282cb20ef0a25a
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-16 19:45:22 -0300

    Adicionado os dados do cartao

--- src/web/ESE.WebApp.MVC/Models/OrderTransactionViewModel.cs
+++ src/web/ESE.WebApp.MVC/Models/OrderTransactionViewModel.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using ESE.Core.Validation;
 
 namespace ESE.WebApp.MVC.Models
 {
@@ -23,6 +21,28 @@ namespace ESE.WebApp.MVC.Models
         #region Endereco
 
         public AddressViewModel Address { get; set; }
+
+        #endregion
+
+        #region Cartão
+
+        [Required(ErrorMessage = "Informe o número do cartão")]
+        [DisplayName("Número do Cartão")]
+        public string NumberCard { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do portador do cartão")]
+        [DisplayName("Nome do Portador")]
+        public string NameCard { get; set; }
+
+        [RegularExpression(@"(0[1-9]|1[0-2])\/[0-9]{2}", ErrorMessage = "O vencimento deve estar no padrão MM/AA")]
+        [CardExpiration(ErrorMessage = "Cartão Expirado")]
+        [Required(ErrorMessage = "Informe o vencimento")]
+        [DisplayName("Data de Vencimento MM/AA")]
+        public string ExpirationCard { get; set; }
+
+        [Required(ErrorMessage = "Informe o código de segurança")]
+        [DisplayName("Código de Segurança")]
+        public string CvvCard { get; set; }
 
         #endregion
 
