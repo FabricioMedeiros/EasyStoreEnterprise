38af7cec69f1874569ff992fa3b9f3bcbcf289d0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-16 19:55:38 -0300

    Criacao da ViewModel de Pedido na WebApp

---
+++ src/web/ESE.WebApp.MVC/Models/OrderViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Models
+{
+    public class OrderViewModel
+    {
+        #region Pedido
+
+        public int Code { get; set; }
+
+        // Autorizado = 1,
+        // Pago = 2,
+        // Recusado = 3,
+        // Entregue = 4,
+        // Cancelado = 5
+        public int Status { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public decimal Discount { get; set; }
+        public bool VoucherUsed { get; set; }
+
+        public List<ItemOrderViewModel> OrderItems { get; set; } = new List<ItemOrderViewModel>();
+
+        #endregion
+
+        #region Item Pedido
+
+        public class ItemOrderViewModel
+        {
+            public Guid ProductId { get; set; }
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+            public decimal Price { get; set; }
+            public string Image { get; set; }
+        }
+
+        #endregion
+
+        #region Endereco
+
+        public AddressViewModel Address { get; set; }
+
+        #endregion
+    }
+}
+}
