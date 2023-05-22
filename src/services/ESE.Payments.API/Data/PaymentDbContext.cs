029b753d9ac3cf23a8864d5f5e1d646f2b83d6a6
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-22 16:22:30 -0300

    Criacao do DbContext da API de Pagamentos

---
+++ src/services/ESE.Payments.API/Data/PaymentDbContext.cs
@@ -0,0 +1,42 @@
+﻿using ESE.Core.Data;
+using ESE.Core.Messages;
+using ESE.Payments.API.Models;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.Payments.API.Data
+{
+    public class PaymentDbContext : DbContext, IUnitOfWork
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Ignore<ValidationResult>();
+            modelBuilder.Ignore<Event>();
+
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PaymentDbContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await SaveChangesAsync() > 0;
+        }
+    }
+}
