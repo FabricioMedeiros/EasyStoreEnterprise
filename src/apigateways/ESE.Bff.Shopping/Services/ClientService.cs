ec02627ce494e9433c2a77c342d5e04417c19c38
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-22 09:10:36 -0300

    Implementacao dos metodos para finalizar o pedido, retornar o ultimo pedido, e retornar a lista de pedidos do cliente na API de Gateway

---
+++ src/apigateways/ESE.Bff.Shopping/Services/ClientService.cs
@@ -0,0 +1,37 @@
+﻿using ESE.Bff.Shopping.Extensions;
+using ESE.Bff.Shopping.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ESE.Bff.Shopping.Services
+{
+    public interface IClientService
+    {
+        Task<AddressDTO> GetAddress();
+    }
+
+    public class ClientService : Service, IClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClientService(HttpClient httpClient, IOptions<AppServicesSettings> settings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(settings.Value.ClientUrl);
+        }
+
+        public async Task<AddressDTO> GetAddress()
+        {
+            var response = await _httpClient.GetAsync("/client/address/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            CheckErrorsResponse(response);
+
+            return await DeserializeObjectResponse<AddressDTO>(response);
+        }
+    }
+}
--- src/apigateways/ESE.Bff.Shopping/Services/OrderService.cs
+++ src/apigateways/ESE.Bff.Shopping/Services/OrderService.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using ESE.Bff.Shopping.Extensions;
 using ESE.Bff.Shopping.Models;
 using ESE.Bff.Shopping.Services;
+using ESE.Core.Communication;
 using Microsoft.Extensions.Options;
 
 namespace ESE.Bff.Shopping.Services
 {
     public interface IOrderService
     {
+        Task<ResponseResult> FinalizeOrder(OrderDTO order);
+        Task<OrderDTO> GetLastOrder();
+        Task<IEnumerable<OrderDTO>> GetListByClientId();
+
         Task<VoucherDTO> GetVoucherByCode(string code);
     }
 
@@ -23,6 +29,40 @@ namespace ESE.Bff.Shopping.Services
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.OrderUrl);
         }
+
+        public async Task<ResponseResult> FinalizeOrder(OrderDTO order)
+        {
+            var orderContent = JsonSerialize(order);
+
+            var response = await _httpClient.PostAsync("/order/", orderContent);
+
+            if (!CheckErrorsResponse(response)) return await DeserializeObjectResponse<ResponseResult>(response);
+
+            return ReturnOk();
+        }
+
+        public async Task<OrderDTO> GetLastOrder()
+        {
+            var response = await _httpClient.GetAsync("/order/last/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            CheckErrorsResponse(response);
+
+            return await DeserializeObjectResponse<OrderDTO>(response);
+        }
+
+        public async Task<IEnumerable<OrderDTO>> GetListByClientId()
+        {
+            var response = await _httpClient.GetAsync("/order/list-client/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            CheckErrorsResponse(response);
+
+            return await DeserializeObjectResponse<IEnumerable<OrderDTO>>(response);
+        }
+
 
         public async Task<VoucherDTO> GetVoucherByCode(string code)
         {
