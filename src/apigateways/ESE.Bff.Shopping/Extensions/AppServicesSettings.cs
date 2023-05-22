9737c42ae9629a8b3958de32c26f4fe6779c3bd1
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-22 09:17:28 -0300

    criacao da property para armazenar a url da API de Clientes na API Gateway

--- src/apigateways/ESE.Bff.Shopping/Extensions/AppServicesSettings.cs
+++ src/apigateways/ESE.Bff.Shopping/Extensions/AppServicesSettings.cs
@@ -4,6 +4,7 @@
     {
         public string CatalogUrl { get; set; }
         public string CartUrl { get; set; }
+        public string ClientUrl { get; set; }
         public string OrderUrl { get; set; }
         public string PaymentUrl { get; set; }
     }
