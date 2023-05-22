91f1cb7cc852427cf4dc9bfc5968a379d43c1d77
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-22 14:58:04 -0300

    Criacao da classe da mensagem de Pagamento que sera enviada via Command

---
+++ src/buildingblocks/ESE.Core/Messages/Integration/OrderStartedIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ESE.Core.Messages.Integration
+{
+    public class OrderStartedIntegrationEvent : IntegrationEvent
+    {
+        public Guid ClientId { get; set; }
+        public Guid OrderId { get; set; }
+        public int TypePayment { get; set; }
+        public decimal Price { get; set; }
+
+        public string NameCard { get; set; }
+        public string NumberCard { get; set; }
+        public string MonthYearExpiry { get; set; }
+        public string CVV { get; set; }
+    }
+}
