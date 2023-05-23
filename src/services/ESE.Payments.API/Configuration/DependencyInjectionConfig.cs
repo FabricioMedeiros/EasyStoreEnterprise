b3243c634123080d09d89c65b206af327a8512f8
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-23 20:41:01 -0300

    Criacao das classes de Configuracao da API, Injecao de Dependencia e do Swagger na API de Pagamentos

---
+++ src/services/ESE.Payments.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using ESE.Payments.API.Data;
+using ESE.WebAPI.Core.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ESE.Payments.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();  
+            
+            services.AddScoped<PaymentDbContext>();
+        }
+    }
+}
