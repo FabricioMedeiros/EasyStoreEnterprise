b42f0f7dbf1a43ab04213e99fac8e62aebebcbd3
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-23 20:37:38 -0300

    Criacao das classes de mapeamento das tabelas de Pagamentos e Transacoes na API de Pagamento

---
+++ src/services/ESE.Payments.API/Data/Mapping/PaymentMapping.cs
@@ -0,0 +1,25 @@
+﻿using ESE.Payments.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ESE.Payments.API.Data.Mapping
+{
+    public class PaymentMapping : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Ignore(c => c.CreditCard);
+
+            // 1 : N => Pagamento : Transacao
+            builder.HasMany(c => c.Transactions)
+                .WithOne(c => c.Payment)
+                .HasForeignKey(c => c.PaymentId);
+
+            builder.ToTable("Payments");
+        }
+       
+    }
+ 
+}
---
+++ src/services/ESE.Payments.API/Data/Mapping/TransactionMapping.cs
@@ -0,0 +1,20 @@
+﻿using ESE.Payments.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ESE.Payments.API.Data.Mapping
+{
+    public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            // 1 : N => Pagamento : Transacao
+            builder.HasOne(c => c.Payment)
+                .WithMany(c => c.Transactions);
+
+            builder.ToTable("Transactions");
+        }
+    }
+}
