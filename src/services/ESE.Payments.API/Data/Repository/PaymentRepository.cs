723fe7c7deacec254de5118145182844384ea620
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-23 20:38:31 -0300

    Criacao da classe de Repositorio de Pagamentos na API de Pagamentos

---
+++ src/services/ESE.Payments.API/Data/Repository/PaymentRepository.cs
@@ -0,0 +1,31 @@
+﻿using ESE.Core.Data;
+using ESE.Payments.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.Payments.API.Data.Repository
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        private readonly PaymentDbContext _context;
+
+        public PaymentRepository(PaymentDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public void AddPayment(Payment payment)
+        {
+            _context.Payments.Add(payment);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
