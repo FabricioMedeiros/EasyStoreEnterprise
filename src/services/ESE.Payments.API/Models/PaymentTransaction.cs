bdb916fd7f86cb6c6732fb9ebb3ff62e29815228
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-29 19:32:34 -0300

    Alteracao do nome da property de Status na transacao do pagamento

--- src/services/ESE.Payments.API/Models/PaymentTransaction.cs
+++ src/services/ESE.Payments.API/Models/PaymentTransaction.cs
@@ -10,7 +10,7 @@ namespace ESE.Payments.API.Models
         public DateTime? TransactionDate { get; set; }
         public decimal TotalPrice { get; set; }
         public decimal Cost { get; set; }
-        public TransactionStatus Status { get; set; }
+        public PaymentTransactionStatus Status { get; set; }
         public string TID { get; set; } // Id
         public string NSU { get; set; } // Meio (paypal)
 
