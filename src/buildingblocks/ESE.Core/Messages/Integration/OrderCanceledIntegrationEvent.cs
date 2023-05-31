35df1a451571151c88294b1067911421598672cf
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-05-31 15:08:42 -0300

    Criacao da classes referentes aos Eventos de Integracao de Pedido Baixado e Pedido Cancelado

---
+++ src/buildingblocks/ESE.Core/Messages/Integration/OrderCanceledIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿using ESE.Core.Messages.Integration;
+using System;
+
+namespace ESE.Core.Messages.Integration
+{
+    public class OrderCanceledIntegrationEvent : IntegrationEvent
+    {
+        public Guid ClientId { get; private set; }
+        public Guid OrderId { get; private set; }
+
+        public OrderCanceledIntegrationEvent(Guid clientId, Guid orderId)
+        {
+            ClientId = clientId;
+            OrderId = orderId;
+        }
+    }
+}
---
+++ src/buildingblocks/ESE.Core/Messages/Integration/OrderDecrementedIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ESE.Core.Messages.Integration
+{
+    public class OrderDecrementedIntegrationEvent : IntegrationEvent
+    {
+        public Guid ClientId { get; private set; }
+        public Guid OrderId { get; private set; }
+
+        public OrderDecrementedIntegrationEvent(Guid clientId, Guid orderId)
+        {
+            ClientId = clientId;
+            OrderId = orderId;
+        }
+    }
+}
