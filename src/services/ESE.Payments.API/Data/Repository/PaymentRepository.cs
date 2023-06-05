9fc99710c4524991073cc04803e2e673522c70cc
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-06-05 10:50:22 -0300

    Criacao dos metodos para adicionar transacao, consultar pagamento pelo id do pedido, e consultar transacoes pelo id do pedido no repositorio de pagamentos da API de Pagamentos

--- src/services/ESE.Payments.API/Data/Repository/PaymentRepository.cs
+++ src/services/ESE.Payments.API/Data/Repository/PaymentRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ESE.Core.Data;
 using ESE.Payments.API.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,23 @@ namespace ESE.Payments.API.Data.Repository
         public void AddPayment(Payment payment)
         {
             _context.Payments.Add(payment);
+        }
+
+        public void AddTransaction(PaymentTransaction paymentTransaction)
+        {
+            _context.Transactions.Add(paymentTransaction);
+        }
+
+        public async Task<Payment> GetPaymentByOrderId(Guid orderId)
+        {
+            return await _context.Payments.AsNoTracking()
+                .FirstOrDefaultAsync(p => p.OrderId == orderId);
+        }
+
+        public async Task<IEnumerable<PaymentTransaction>> GetTransactionsByOrderId(Guid orderId)
+        {
+            return await _context.Transactions.AsNoTracking()
+                .Where(t => t.Payment.OrderId == orderId).ToListAsync();
         }
 
         public void Dispose()
--- src/services/ESE.Payments.API/Models/IPaymentRepository.cs
+++ src/services/ESE.Payments.API/Models/IPaymentRepository.cs
@@ -1,7 +1,6 @@
 ﻿using ESE.Core.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ESE.Payments.API.Models
@@ -9,5 +8,8 @@ namespace ESE.Payments.API.Models
     public interface IPaymentRepository : IRepository<Payment>
     {
         void AddPayment(Payment payment);
+        void AddTransaction(PaymentTransaction paymentTransaction);
+        Task<Payment> GetPaymentByOrderId(Guid orderId);
+        Task<IEnumerable<PaymentTransaction>> GetTransactionsByOrderId(Guid orderId);
     }
 }
