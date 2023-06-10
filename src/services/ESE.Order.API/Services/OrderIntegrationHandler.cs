cfa9819e63dc6be770e3087c16526c54bd6b8e71
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-06-10 14:34:30 -0300

    Criacao da classe Handler de Pedidos na API de Pedidos

---
+++ src/services/ESE.Order.API/Services/OrderIntegrationHandler.cs
@@ -0,0 +1,72 @@
+﻿using ESE.Core.DomainObjects;
+using ESE.Core.Messages.Integration;
+using ESE.MessageBus;
+using ESE.Orders.Domain.Orders;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESE.Orders.API.Services
+{
+    public class OrderIntegrationHandler : BackgroundService
+    {
+        private readonly IMessageBus _bus;
+        private readonly IServiceProvider _serviceProvider;
+
+        public OrderIntegrationHandler(IServiceProvider serviceProvider, IMessageBus bus)
+        {
+            _serviceProvider = serviceProvider;
+            _bus = bus;
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            SetSubscribers();
+            return Task.CompletedTask;
+        }
+
+        private void SetSubscribers()
+        {
+            _bus.SubscribeAsync<OrderCanceledIntegrationEvent>("OrderCanceled", async request => await CancelOrder(request));
+
+            _bus.SubscribeAsync<OrderPaidIntegrationEvent>("OrderPaid", async request => await FinalizeOrder(request));
+        }
+
+        private async Task CancelOrder(OrderCanceledIntegrationEvent message)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var orderRepository = scope.ServiceProvider.GetRequiredService<IOrderRepository>();
+
+                var order = await orderRepository.GetById(message.OrderId);
+                order.SetCanceledOrder();
+
+                orderRepository.Update(order);
+
+                if (!await orderRepository.UnitOfWork.Commit())
+                {
+                    throw new DomainException($"Problemas ao cancelar o pedido {message.OrderId}");
+                }
+            }
+        }
+
+        private async Task FinalizeOrder(OrderPaidIntegrationEvent message)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var orderRepository = scope.ServiceProvider.GetRequiredService<IOrderRepository>();
+
+                var order = await orderRepository.GetById(message.OrderId);
+                order.SetPaidOrder();
+
+                orderRepository.Update(order);
+
+                if (!await orderRepository.UnitOfWork.Commit())
+                {
+                    throw new DomainException($"Problemas ao finalizar o pedido {message.OrderId}");
+                }
+            }
+        }
+    }
+}
