76070a67b1cbec1fc5a9b1937ae5a3309ee5de68
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-06-13 20:40:39 -0300

    Criacao da ViewComponent de Paginacao na WebApp

---
+++ src/web/ESE.WebApp.MVC/Extensions/PaginationViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using ESE.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESE.WebApp.MVC.Extensions
+{
+    public class PaginationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IPagedList modelPagined)
+        {
+            return View(modelPagined);
+        }
+    }
+}
