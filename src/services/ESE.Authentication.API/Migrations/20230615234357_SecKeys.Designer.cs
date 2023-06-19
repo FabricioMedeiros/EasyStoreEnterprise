3dd97dd7ff1fd8120c02bde9a054910353e9bcfe
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-06-19 20:51:17 -0300

    Refatoracao para criacao da Migration de SecurityKey na API de Autenticacao

--- src/services/ESE.Authentication.API/Migrations/20230615234357_SecKeys.Designer.cs
+++ src/services/ESE.Authentication.API/Migrations/20230615234357_SecKeys.Designer.cs
@@ -4,14 +4,16 @@ using ESE.Authentication.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ESE.Authentication.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230615234357_SecKeys")]
+    partial class SecKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
